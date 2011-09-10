4aa9cc66db6d52ac3d44b7fdf7df483fa9d2ee45
Tobias <tobias@gnu.eti.br>
2011-09-10 17:33:16 -0300

    Ignorado alguns arquivos de preferência do editor que utilizo. Criado arquivo README

---
+++ app/config/configuracoes.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @author        Tobias Sette Ferreira
+ * @copyright     Tobias Sette Ferreira - 2011
+ * @link          http://gnu.eti.br
+ * @package       GFreedom
+ * @license       GPL v3
+ */
+
+// Configurações da aplicação
+$config['app']['nome'] = 'Gfreedom';
+$config['app']['slogan'] = 'Simples e poderoso';
+
+//Configurações do cliente
+$config['cliente']['nome'] = 'GNU Corporation';
+$config['cliente']['logo'] = '';
+
+
+
+?>
